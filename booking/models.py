b1d800cd760eb216e8d610cd7c0859698f0c5a7d
MateuszPestka <pes.matthew@gmail.com>
2022-10-30 20:57:03 +0000

    Add model for booking

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Booking(models.Model):
+    first_name = models.charField("your first name", max_length=100)
+    last_name = models.charField("your last name", max_length=100)
+    email = models.EmailField()
+    mobile = models.IntegerField(max_length=13)
+    created_on = models.DateTimeField(auto_now_add=True)
+    date = models.DateField()
+    time = models.TimeField()
+    special_requirements = models.TextField()
