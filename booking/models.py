bf2d8760cea1ea8e075e8b92f8322e4bb3c7466f
MateuszPestka <pes.matthew@gmail.com>
2022-12-03 13:17:05 +0000

    Test new database

--- booking/models.py
+++ booking/models.py
@@ -1,13 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
 class Booking(models.Model):
-    first_name = models.charField("your first name", max_length=100)
-    last_name = models.charField("your last name", max_length=100)
+    first_name = models.CharField("your first name", max_length=100)
+    last_name = models.CharField("your last name", max_length=100)
     email = models.EmailField()
-    mobile = models.IntegerField(max_length=13)
+    mobile = models.IntegerField()
     created_on = models.DateTimeField(auto_now_add=True)
     date = models.DateField()
     time = models.TimeField()
