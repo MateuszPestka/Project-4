49763084e057c0fae7181004c55bfd45b40ba356
MateuszPestka <pes.matthew@gmail.com>
2022-12-08 12:54:32 +0000

    Add booking admin class

--- booking/admin.py
+++ booking/admin.py
@@ -2,3 +2,11 @@ from django.contrib import admin
 from .models import Booking
 
 admin.site.register(Booking)
+
+
+class BookingAdmin(admin.ModelAdmin):
+
+    list_display = ('date', 'time', 'first_name', 'last_name', 'approved', 'vip')
+    search_fields = ['date', 'time', 'first_name', 'last_name']
+    list_filter = ('approved', 'vip')
+    actions = ['approve_booking', 'make_vip']
