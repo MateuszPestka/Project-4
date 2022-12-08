6aa2c877239a63aa6b801681a4408ca77e406065
MateuszPestka <pes.matthew@gmail.com>
2022-12-08 12:32:48 +0000

    Add VIP and group size to Booking model

--- booking/models.py
+++ booking/models.py
@@ -5,15 +5,20 @@ from django.contrib.auth.models import User
 
 
 class Booking(models.Model):
-    first_name = models.CharField("your first name", max_length=100)
-    last_name = models.CharField("your last name", max_length=100)
+    first_name = models.CharField("your first name", max_length=50)
+    last_name = models.CharField("your last name", max_length=50)
     email = models.EmailField()
     mobile = models.IntegerField()
     created_on = models.DateTimeField(auto_now_add=True)
     date = models.DateField()
     time = models.TimeField()
-    special_requirements = models.TextField()
+    group_size = models.PositiveIntegerField(default=1)
+    special_requirements = models.TextField(blank=True)
     approved = models.BooleanField(default=False)
+    vip = models.BooleanField(default=False)
 
     class Meta:
         ordering = ["time"]
+
+    def __str__(self):
+        return f"{self.time}: {self.first_name} {self.last_name}"
