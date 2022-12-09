3f2b00a9fdf82face8d7664048c6b7e03ce4f6b4
MateuszPestka <pes.matthew@gmail.com>
2022-12-09 01:28:35 +0000

    Add views for booking

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Booking
 
 # Create your views here.
+
+
+class BookingList(generic.ListView):
+    model = Booking
+    queryset = Booking.objects.filter(approved=True).order_by("time")
+    template_name = "index.html"
