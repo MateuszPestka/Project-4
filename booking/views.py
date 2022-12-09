2b4020040cc7a1ef3053c4074ac87aab49678681
MateuszPestka <pes.matthew@gmail.com>
2022-12-09 10:41:41 +0000

    Add get to bookingDetail

--- booking/views.py
+++ booking/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views import generic
+from django.views import generic, View
+from django.http import HttpResponseRedirect
 from .models import Booking
 
 # Create your views here.
@@ -9,3 +10,18 @@ class BookingList(generic.ListView):
     model = Booking
     queryset = Booking.objects.filter(approved=True).order_by("time")
     template_name = "index.html"
+
+
+class BookingDetail(View):
+
+    def get(self, request, first_name, *args, **kwargs):
+        queryset = Booking().objects.filter(approved=True)
+        booking = get_object_or_404(queryset, first_name=first_name)
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "booking": booking,
+            },
+        )
